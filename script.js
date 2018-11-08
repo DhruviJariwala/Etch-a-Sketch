bb14608c4e9a04952ba09bdbe3ca709f40449217
Dhruvi Jariwala <dhruvi.guard195@gmail.com>
2018-11-08 15:45:22 -0500

    Clean div hover bug

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 /*-------- HTML element references -------- */
-const CONTAINER = document.querySelector(".container");
+const CONTAINER = document.querySelector(".CONTAINER");
 const BUTTON = document.querySelector("button");
-let div;
+let squareDiv, userInput, r, c;
 
-/*-------- Creating 16*16 grid -------- */
+/*-------- Creating 16*16 CONTAINER ---x----- */
 for(i=0;i<16;i++){
     for(j=0;j<16;j++){
-        div = document.createElement("div");
-        div.style.cssText = "height: 50px; width: 50px; border: 1px solid rgba(170, 68, 100, 0.692);; display: inline-block;";
-        CONTAINER.appendChild(div);
+        squareDiv = document.createElement("div");
+        squareDiv.style.cssText = "height: 50px; width: 50px; border: 1px solid rgba(170, 68, 100, 0.692);; display: inline-block;";
+        CONTAINER.appendChild(squareDiv);
     }
     CONTAINER.innerHTML+= "<br/>";
 }
 
 /*-------- Hover Effects -------- */
-CONTAINER.addEventListener("mouseover", function(e){
+document.addEventListener("mouseover", function(e){
+    if(e.target.tagName == "DIV"){
+        if(e.srcElement.id=="iddiv" || e.srcElement.className=="header"){
+            console.log(e.srcElement.id);
+            return;
+        }
+        else{
+            e.target.style.backgroundColor = "brown";
+            console.log(e.srcElement.id);
+        }
+    }
+    else{
+        return;
+    }
+})
+/*CONTAINER.addEventListener("mouseover", function(e){
     e.target.style.backgroundColor = "brown";
-});
+    console.log(e.target);
+});*/
 
 /*-------- Clear Effects -------- */
 BUTTON.addEventListener("click", function(){
     location.reload();
+    userInput = window.prompt("How many squares you want on a grid?");
 });
