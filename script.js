f71bef61eddb2aafdb8d206b30e31b02eaf92929
Dhruvi Jariwala <dhruvi.guard195@gmail.com>
2018-11-08 13:40:47 -0500

    Add clear button

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 /*-------- HTML element references -------- */
 const CONTAINER = document.querySelector(".container");
+const BUTTON = document.querySelector("button");
 let div;
 
 /*-------- Creating 16*16 grid -------- */
 for(i=0;i<16;i++){
     for(j=0;j<16;j++){
         div = document.createElement("div");
-        div.style.cssText = "height: 50px; width: 50px; border: 1px solid blue; display: inline-block;";
+        div.style.cssText = "height: 50px; width: 50px; border: 1px solid rgba(170, 68, 100, 0.692);; display: inline-block;";
         CONTAINER.appendChild(div);
     }
     CONTAINER.innerHTML+= "<br/>";
@@ -15,4 +16,9 @@ for(i=0;i<16;i++){
 /*-------- Hover Effects -------- */
 CONTAINER.addEventListener("mouseover", function(e){
     e.target.style.backgroundColor = "brown";
+});
+
+/*-------- Clear Effects -------- */
+BUTTON.addEventListener("click", function(){
+    location.reload();
 });
