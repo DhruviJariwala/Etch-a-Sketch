7a287e207579060287568d3e1c74f36778f3872a
Dhruvi Jariwala <dhruvi.guard195@gmail.com>
2018-11-15 13:26:23 -0500

    Add header and box shadow

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 /*-------- HTML element references -------- */
 const CONTAINER = document.querySelector(".CONTAINER");
 const BUTTON = document.querySelector("button");
-let squareDiv, r, c, i, j, number;
+let squareDiv, r, c, i, j, number, userInput;
 
-/*-------- Default grid ---x----- */
-function getStarted(){
-    clearGrid();
-    createGrid();
+/*-------- Default Grid --------- */
+function defaultGrid(){
+    createGrid(16);
 }
 
-/*-------- Clear grid ---x----- */
+/*-------- Get Started --------- */
+function getStarted(){
+    clearGrid();
+    askInput();
+    checkGrid(userInput);
+}
+
+/*-------- Clear grid -------- */
 function clearGrid(){
     while (CONTAINER.firstChild) {
         CONTAINER.removeChild(CONTAINER.firstChild);
     }
 }
 
-/*-------- Creating grid CONTAINER ---x----- */
-function createGrid(){
-    number = prompt("Enter number between 2 to 100: ");
-    CONTAINER.style.gridTemplateColumns = "repeat("+number+", 1fr)";
-    CONTAINER.style.gridTemplateRows = "repeat("+number+", 1fr)";
-    let mul = number*number;
+/*-------- Check grid -------- */
+function checkGrid(number){
+    if(number === "" || isNaN(number) || number<=0)
+    {
+        if(number == null){
+            defaultGrid();
+        }
+        else{
+            alert("You entered something else!!!! Try again!");
+            defaultGrid();
+        }
+    }
+    else{
+        createGrid(number);
+    }
+}
+
+/*-------- Ask for Input -------- */
+function askInput(){
+    userInput = prompt("Enter a number between 2 to 100: ");
+}
+
+
+/*-------- Creating grid CONTAINER -------- */
+function createGrid(n){
+    CONTAINER.style.gridTemplateColumns = "repeat("+n+", 1fr)";
+    CONTAINER.style.gridTemplateRows = "repeat("+n+", 1fr)";
+    let mul = n*n;
     for(i=1;i<=mul;i++){
         squareDiv = document.createElement("div");
         squareDiv.setAttribute("class","child");
@@ -32,7 +60,7 @@ function createGrid(){
 /*-------- Hover Effects -------- */
 function hoverEffect(e){
     if(e.target.tagName == "DIV"){
-        if(e.srcElement.id=="iddiv" || e.srcElement.className=="header" || e.srcElement.className=="clear-button"){
+        if(e.srcElement.id=="iddiv" || e.srcElement.className=="header" || e.srcElement.className=="clear-button" || e.srcElement.className=="choose-button"){
             return;
         }
         else{
