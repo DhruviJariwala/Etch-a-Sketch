a2303628b8a884787f27bfec5580d4279b75b688
Dhruvi Jariwala <dhruvi.guard195@gmail.com>
2018-11-15 16:17:24 -0500

    Change background-color

--- script.js
+++ script.js
@@ -69,6 +69,35 @@ function getRandomColor() {
     return color;
   }
 
+/*-------- Lighten/Darken Colors -------- */
+function LightenDarkenColor(colorCode, amount) {
+    var usePound = false;
+    if (colorCode[0] == "#") {
+        colorCode = colorCode.slice(1);
+        usePound = true;
+    }
+    var num = parseInt(colorCode, 16);
+    var r = (num >> 16) + amount;
+    if (r > 255) {
+        r = 255;
+    } else if (r < 0) {
+        r = 0;
+    }
+    var b = ((num >> 8) & 0x00FF) + amount;
+    if (b > 255) {
+        b = 255;
+    } else if (b < 0) {
+        b = 0;
+    }
+    var g = (num & 0x0000FF) + amount;
+    if (g > 255) {
+        g = 255;
+    } else if (g < 0) {
+        g = 0;
+    }
+    return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
 /*-------- Hover Effects -------- */
 function hoverEffect(e){
     if(e.target.tagName == "DIV"){
@@ -77,6 +106,8 @@ function hoverEffect(e){
         }
         else{
             let rc = getRandomColor();
+            console.log(rc);
+            console.log(LightenDarkenColor(rc,-20));
             e.target.style.backgroundColor = rc;
         }
     }
