79d7b3c004c0887fcb76cb77323c256f398dd0ac
Dhruvi Jariwala <dhruvi.guard195@gmail.com>
2018-11-19 10:39:21 -0500

    Add rainbow and black buttons

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 const CONTAINER = document.querySelector(".CONTAINER");
 const CLEARBUTTON = document.querySelector(".cl-button");
 const CHOOSEBUTTON = document.querySelector(".ch-button");
+const RAINBOWBUTTON = document.querySelector(".ra-button");
+const BWBUTTON = document.querySelector(".bw-button");
 let squareDiv, r, c, i, j, number, userInput;
+let hsl = "hsl(0, 0%, 81%)";
 
 /*-------- Default Grid --------- */
 function defaultGrid(){
@@ -71,21 +74,51 @@ function getRandomColor() {
 
 /*-------- Hover Effects -------- */
 function hoverEffect(e){
-    if(e.target.tagName == "DIV"){
-        if(e.srcElement.id=="iddiv" || e.srcElement.className=="header" || e.srcElement.className=="clear-button" || e.srcElement.className=="choose-button"){
-            return;
-        }
-        else{
-            let rc = getRandomColor();
-            e.target.style.backgroundColor = rc;
-        }
-    }
-    else{
+    let rc = getRandomColor();
+    e.target.style.backgroundColor = rc;
+}
+CONTAINER.addEventListener("mouseover", hoverEffect);
+
+/*-------- Black and White Hover Effect -------- */
+function darken(e){
+    let dec = hsl.slice(11,14);
+    let decN = parseInt(dec);
+    if(decN < 0){
         return;
     }
+    decN = decN - 10;
+    decN = decN.toString();
+    hsl = "hsl(0, 0%, "+decN+"%)";
+    e.target.style.backgroundColor = hsl;
+    console.log(hsl);
 }
-document.addEventListener("mouseover", hoverEffect);
 
+/*-------- Black and White Effect -------- */
+let ba = false;
+let ra = false;
+function blacke(){
+    ra = false;
+    ba = true;
+    console.log("Inside black"+"\nra "+ra+"\nba "+ba);
+    CONTAINER.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "black";
+        console.log(e.target.style.backgroundColor);
+    });
+}
+BWBUTTON.addEventListener("click", blacke);
+
+/*-------- Rainbow Effect -------- */
+function rainbow(){
+    ra = true;
+    ba = false;
+    console.log("Inside Rainbow"+"\nra "+ra+"\nba "+ba);
+    CONTAINER.addEventListener("mouseover", (e) => {
+        let rc = getRandomColor();
+        e.target.style.backgroundColor = rc;
+        console.log(e.target.style.backgroundColor);
+    });
+}
+RAINBOWBUTTON.addEventListener("click", rainbow);
 
 /*-------- Clear Hover Effects -------- */
 CONTAINER.addEventListener("mousemove", (e) => {
@@ -103,6 +136,7 @@ CLEARBUTTON.addEventListener("click", function(e){
     {
         clearGrid();
         defaultGrid();
+        CONTAINER.addEventListener("mouseover", hoverEffect);
     }
 });
 
